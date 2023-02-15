625832cb6f6079dd0c6cc73ff0ecc3b0a91be43f
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-15 18:33:18 +0530

    Update pattern4.py

--- Python pattern/pattern4.py
+++ Python pattern/pattern4.py
@@ -11,7 +11,7 @@ b = 5
 
 #outer loop to handle rows
 for i in range(rows, 0, -1):
-    #inner loop to print 5 from 1 to i ti es
+    #inner loop to print 5 from 1 to i times
     for j in range(1, i + 1):
         print(b, end=" ")
 
