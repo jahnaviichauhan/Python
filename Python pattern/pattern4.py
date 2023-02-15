1d920d5c5835c8a340b6219a621fa5382a2257f8
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-15 18:24:41 +0530

    Add files via upload

---
+++ Python pattern/pattern4.py
@@ -0,0 +1,18 @@
+# To print the following pattern
+# 5 5 5 5 5 
+# 5 5 5 5 
+# 5 5 5 
+# 5 5 
+# 5 
+
+#To input number of rows
+rows=int(input("Enter number of rows:"))
+b = 5
+
+#outer loop to handle rows
+for i in range(rows, 0, -1):
+    #inner loop to print 5 from 1 to i ti es
+    for j in range(1, i + 1):
+        print(b, end=" ")
+
+    print("\r")
