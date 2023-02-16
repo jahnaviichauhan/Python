fcfbae595c7634cc8b3b6f897858c894e3725c3b
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-16 12:27:05 +0530

    Add files via upload
    enter first number4
    enter second number5
    Numbers are: 4.0 5.0
    Swapped numbers are: 5.0 4.0

---
+++ Number programs/classswap.py
@@ -0,0 +1,17 @@
+#Program to swap two numbers
+
+#Inputting the two numbers
+n1=float(input("enter first number"))
+n2=float(input("enter second number"))
+
+#Printing actual values
+print("Numbers are:", n1, n2)
+
+#Swapping-
+
+a=n1
+n1=n2
+n2=a
+
+#Printing swapped values
+print("Swapped numbers are:", n1, n2)
