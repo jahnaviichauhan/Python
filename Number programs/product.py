e7deb98f15e5ff9d8d31772fdd015d0fedcb89bc
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-16 11:58:02 +0530

    Add files via upload
    Enter first number
    4
    Enter second number
    3
    Product: 12

---
+++ Number programs/product.py
@@ -0,0 +1,12 @@
+#Multiplication of two numbers
+
+#input of first and second number and type casting them
+print("Enter first number")
+n1=int(input())
+print("Enter second number")
+n2=int(input())
+
+ans=n1*n2
+
+#Printing of value 
+print("Product:", ans)
