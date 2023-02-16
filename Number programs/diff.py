440f3640873034f49eca6cccf09c86703eb93995
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-16 11:54:55 +0530

    16/2/23
    Enter first number
    6
    Enter second number
    8
    Difference: 2

---
+++ Number programs/diff.py
@@ -0,0 +1,15 @@
+#Subtraction of two numbers
+
+#input of first and second number and type casting them
+print("Enter first number")
+n1=int(input())
+print("Enter second number")
+n2=int(input())
+
+if(n1>n2):
+    diff=n1-n2
+else:
+    diff=n2-n1
+
+#Printing value of difference
+print("Difference:", diff)
