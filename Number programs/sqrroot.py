7afcf6240229a8fde2fcd4d0d5beddc084a0365d
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-16 12:34:01 +0530

    Add files via upload
    Enter the number
    25
    Sqaureroot of 25 is 5.0

---
+++ Number programs/sqrroot.py
@@ -0,0 +1,12 @@
+#Printing squareroot of a number
+
+#Importing math library
+import math
+
+print("Enter the number")
+n=int(input())
+
+#Calculation of square root using math.sqrt 
+ans=math.sqrt(n)
+
+print("Sqaureroot of {0} is {1}". format(n,ans) )
