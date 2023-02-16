46395744192c96712c9ba20aa27aaf37d5ee27bb
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-16 12:21:39 +0530

    Add files via upload
    Enter first number
    5
    Enter second number
    4
    Average: 4.5

---
+++ Number programs/avg.py
@@ -0,0 +1,12 @@
+#Average of n numbers
+
+#input of first and second number and type casting them
+print("Enter first number")
+n1=int(input())
+print("Enter second number")
+n2=int(input())
+
+avg=(n1+n2)/2
+
+#Printing value of average
+print("Average:", avg)
