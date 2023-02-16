27c85e5fc8b23b8080bc4a6047d39d55c86d04f1
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-16 11:51:00 +0530

    Add files via upload
    Enter first number
    4
    Enter second number
    5
    Sum: 9

--- Number programs/add.py
+++ Number programs/add.py
@@ -1,1 +1,13 @@
+#Addition of two numbers
 
+#input of first and second number and type casting them
+print("Enter first number")
+n1=int(input())
+print("Enter second number")
+n2=int(input())
+
+
+sum=n1+n2
+
+#Printing value of sum
+print("Sum:", sum)
