8817dbcb79348759bd45d6fc7e779c89d9e80a1c
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-16 21:06:42 +0530

    Area of rectangle
    Enter length of rectangle
    4
    Enter breadth of rectangle
    4
    Area of rectangle: 16.0

---
+++ Number programs/rectangle_area.py
@@ -0,0 +1,12 @@
+# Program to calculate area of rectangle
+
+#Inputting length and breadth of rectangle
+print("Enter length of rectangle")
+l=float(input())
+print("Enter breadth of rectangle")
+b=float(input())
+
+#area= length*breadth
+area=l*b
+
+print("Area of rectangle:", area)
