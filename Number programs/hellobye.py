71e47e371ae9f0c25e82683694ba53ae1a5a5fd8
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-22 19:19:47 +0530

    Add files via upload
    Printing hello if number entered is divisible by 5 otherwise printing bye

---
+++ Number programs/hellobye.py
@@ -0,0 +1,9 @@
+#Printing "Hello" if number entered by user is multiple of 5, otherwise print "Bye"
+
+#Inputting a number
+n=int(input("Enter number:"))
+
+if n%5==0:
+    print("Hello")
+else:
+    print("Bye")
