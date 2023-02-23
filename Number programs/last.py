be6794befc467214beebc9a42d95b59c930d972e
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-23 11:50:24 +0530

    Add files via upload

---
+++ Number programs/last.py
@@ -0,0 +1,10 @@
+#Program to display last digit of any number
+
+#Inputting a number
+n=int(input("Enter number"))
+
+#Finding last digit of number
+d=n%10
+
+#Printing last digit of number
+print("Last digit of {0} is {1}".format(n, d))
