816d8635561b1bf51f4256f263332b4d17a5dfbb
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-23 11:53:16 +0530

    Add files via upload

---
+++ Number programs/tarea.py
@@ -0,0 +1,11 @@
+#Calculation of area of triangle
+
+#Inputting base and height of triangle
+print("Enter base of triangle")
+b=float(input())
+print("Enter height of triangle")
+h=float(input())
+
+area=0.5*b*h
+
+print("Area:", area)
