d575fb5b9b7e289e07e2e28e63455afeed5a4fdb
Jahnavi Singh Chauhan <jahnavi.102342@stu.upes.ac.in>
2023-02-23 11:51:42 +0530

    Add files via upload

---
+++ Number programs/oddeven.py
@@ -0,0 +1,11 @@
+# Program to check if number entered is even or odd
+
+#Inputting a number
+n=int(input("Enter number:"))
+
+#Checking if number is odd or even
+if n%2==0:
+    print("Number is even.")
+else:
+    print("Number is odd.")
+    
